c42bbe61658bcc9a26538db3eac9931639b272eb
Kayson Yusuph <yusuphkayxn@gmail.com>
2020-06-01 15:27:47 +0300

    Working with anular life hooks

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, ContentChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
-  @Input('srvElement') element: {type: string, name: string, content: string};
+export class ServerElementComponent implements OnInit, OnChanges, AfterViewInit {
+  // @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input('srvName') name: string;
+  @ContentChild('paragraphContent') paragraph: ElementRef;
 
   constructor() { }
 
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('Changed');
+    console.log(changes);
+  }
+
   ngOnInit(): void {
+    console.log('Paragraph1 is ' + this.paragraph.nativeElement.textContent);
+  }
+
+  ngAfterViewInit() {
+    console.log('Paragraph2 is ' + this.paragraph.nativeElement.textContent);
   }
 
 }
