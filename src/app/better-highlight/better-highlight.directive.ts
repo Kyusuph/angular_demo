08228a9f3cf1d11ad94b9d92d1fbfec2612f804f
Kayson Yusuph <yusuphkayxn@gmail.com>
2020-06-02 22:16:22 +0300

    Modifying attribute directives

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,13 +1,26 @@
-import { Directive, ElementRef, Renderer2, OnInit } from '@angular/core';
+import { Directive, ElementRef, Renderer2, OnInit, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 
 export class BetterHighlightDirective implements OnInit {
-constructor(private elRef: ElementRef, private renderer: Renderer2) {}
+  @HostBinding('style.backgroundColor') backgroundColor = 'yellow';
+  @Input('appBetterHighlight') defaultColor = 'yellow';
+  @Input() highlightColor = 'red';
+  @HostListener('mouseleave') mouseleave() {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', this.defaultColor);
+    this.backgroundColor = this.defaultColor;
+  }
+  @HostListener('mouseenter') mouseenter() {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', this.highlightColor);
+    this.backgroundColor = this.highlightColor;
+  }
+  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
-ngOnInit() {
-  this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'yellow');
-}
+  ngOnInit() {
+    console.log(this.highlightColor);
+    this.backgroundColor = this.defaultColor;
+    // this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'yellow');
+  }
 }
