3ff5b421ae94159601cfbca00dfaed889c58e481
Kayson Yusuph <yusuphkayxn@gmail.com>
2020-06-02 15:43:07 +0300

    Initializing for directive modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,53 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  score = 0;
-lastScore = 0;
-  stopped = true;
-  serverElements = [{type: 'server', name: 'Test server', content: 'Just a server'}];
-  oddScores: number[] = [];
-  evenScores: number[] = [];
-
-  onServerCreated(serverData: {serverName: string, serverContent: string}) {
-    this.serverElements.push({
-      type: 'server',
-      name: serverData.serverName,
-      content: serverData.serverContent
-    });
-  }
-
-  onBlueprintCreated(serverData: {serverName: string, serverContent: string}) {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: serverData.serverName,
-      content: serverData.serverContent
-    });
-  }
-
-  onFirstChange() {
-    this.serverElements[0].name = 'Changed';
-  }
-
-  onStarted(score: number) {
-    if(this.isOdd(score)) {
-      this.oddScores.push(score);
-    } else {
-      this.evenScores.push(score);
-    }
-    this.stopped = false;
-    this.score = score;
-  }
-
-  onStopped(score: number) {
-    this.stopped = true;
-    this.lastScore = score;
-  }
-
-  isOdd(value: number): boolean {
-    let toReturn = true;
-    if(value % 2 === 0) {
-      toReturn = false;
-    }
-    return toReturn;
-  }
+  numbers = [1, 2, 3, 4, 5];
+  onlyOdd = false;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { CockpitComponent } from './cockpit/cockpit.component';
-import { ServerElementComponent } from './server-element/server-element.component';
-import { GameControlComponent } from './game-control/game-control.component';
-import { OddComponent } from './odd/odd.component';
-import { EvenComponent } from './even/even.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CockpitComponent,
-    ServerElementComponent,
-    GameControlComponent,
-    OddComponent,
-    EvenComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
