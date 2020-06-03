7c2d7c7e2509daabbf8fea1856f917c7449e5c81
Kayson Yusuph <yusuphkayxn@gmail.com>
2020-06-03 13:56:55 +0300

    Injecting service inside a service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AccountComponent } from './account/account.component';
 import { NewAccountComponent } from './new-account/new-account.component';
+import { LoggingService } from './service/logging.service';
+import { AccountService } from './service/account.service';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,7 @@ import { NewAccountComponent } from './new-account/new-account.component';
     BrowserModule,
     FormsModule,
   ],
-  providers: [],
+  providers: [AccountService, LoggingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/service/account.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+
+import { LoggingService } from './logging.service';
+
+@Injectable()
+export class AccountService {
+  accounts = [
+    {
+      name: 'Master Account',
+      status: 'active'
+    },
+    {
+      name: 'Testaccount',
+      status: 'inactive'
+    },
+    {
+      name: 'Hidden Account',
+      status: 'unknown'
+    }
+  ];
+  constructor(private loggingService: LoggingService) {}
+
+  addAccount(name: string, status: string) {
+    this.accounts.push({name, status});
+    this.loggingService.logStatusChange(status);
+  }
+
+  updateStatus(id: number, newStatus: string) {
+    this.accounts[id].status = newStatus;
+    this.loggingService.logStatusChange(newStatus);
+  }
+
+  getAllAccounts() {
+    return this.accounts;
+  }
+}
