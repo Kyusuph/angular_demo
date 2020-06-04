79a5ee1622a319a5be0a9125cad1c53c90b9a09f
Kayson Yusuph <yusuphkayxn@gmail.com>
2020-06-04 06:41:15 +0300

    Initializing working env for routing session

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-
-import { UserService } from './users.service';
-import { CenterService } from './center.service';
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  actvCount;
-  inActvCount;
-
-  constructor(
-    private centerService: CenterService,
-    private userService: UserService
-  ) { }
-
-  ngOnInit() {
-    this.actvCount = this.centerService.activationCount;
-    this.inActvCount = this.centerService.deActivationCount;
-  }
+export class AppComponent {
 }
