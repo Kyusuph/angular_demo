340f29c6e0f6913b307db3353030b40201155581
Kayson Yusuph <yusuphkayxn@gmail.com>
2020-06-07 18:36:28 +0300

    Setting up environment for the form session

--- src/app/user.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable, EventEmitter } from '@angular/core';
-import { Subject } from 'rxjs';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class UserService {
-  activateParagraph = new Subject<boolean>();
-}
