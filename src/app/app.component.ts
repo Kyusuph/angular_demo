694c4c1b14734488f405f59d9eca6af60393296a
Kayson Yusuph <yusuphkayxn@gmail.com>
2020-06-08 06:47:50 +0300

    Adding and hanling gender selection with radio buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,8 @@ export class AppComponent {
   @ViewChild('f') signUpForm: NgForm;
   defaultQuestion = 'pet';
   questionAnswer = '';
+  genders = ['male', 'female'];
+  startingGender = 'male';
 
 
   suggestUserName() {
