4119c82491299487a4ba72a45fd87a6ae2d7519a
Kayson Yusuph <yusuphkayxn@gmail.com>
2020-07-02 12:04:12 +0300

    Adding and confirguring a filter pipe

---
+++ src/app/filter.pipe.ts
@@ -0,0 +1,18 @@
+import { PipeTransform, Pipe } from '@angular/core';
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+  transform(value: any, filterValue: string, searchField: string) {
+    const filterResult = [];
+    if (!value.length || !filterValue) {
+      return value;
+    }
+    for (const item of value) {
+      if (item[searchField].includes(filterValue)) {
+        filterResult.push(item);
+      }
+    }
+    return filterResult;
+  }
+}
