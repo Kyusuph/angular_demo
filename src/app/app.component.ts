5aa7a3308225214d65611940f8973c5355b4e3e1
Kayson Yusuph <yusuphkayxn@gmail.com>
2020-07-03 06:47:50 +0300

    Posting and getting data from backend with http request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { importExpr } from '@angular/compiler/src/output/output_ast';
+import { pipe } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -8,19 +12,44 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   loadedPosts = [];
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
+    this.http.post('https://demoz-app.firebaseio.com/posts.json', postData)
+        .subscribe((response) => {
+          console.log(response);
+        },
+        (error) => {
+          console.error('Failed to save post: ', error.message);
+        });
   }
 
   onFetchPosts() {
-    // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  fetchPosts() {
+    this.http.get('https://demoz-app.firebaseio.com/posts.json')
+      .pipe(map(response => {
+        const responseArray = [];
+        for (const key in response) {
+          if (response.hasOwnProperty(key)) {
+            responseArray.push({...response[key], id: key});
+          }
+        }
+        return responseArray;
+      }))
+      .subscribe((response) => {
+      console.log(response);
+    });
   }
 }
