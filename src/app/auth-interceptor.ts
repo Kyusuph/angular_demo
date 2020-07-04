e878cee1c699533dacac4f3ad62bd10f64b1423d
Kayson Yusuph <yusuphkayxn@gmail.com>
2020-07-04 12:53:55 +0300

    Retrieving query params and headers using interceptor

--- src/app/auth-interceptor.ts
+++ src/app/auth-interceptor.ts
@@ -2,7 +2,13 @@ import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http'
 
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
-    console.log('Am leaving now!');
+    const keys = req.params.keys();
+    if( keys ) {
+      for (const key of keys) {
+        console.log(key, req.params.get(key));
+      }
+    }
+    console.log('Am leaving now: ' + JSON.stringify(req.headers.has('name')));
     return next.handle(req);
   }
 }
