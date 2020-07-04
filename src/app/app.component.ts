97486d49d0b4787f6606a544009322d2e78210b5
Kayson Yusuph <yusuphkayxn@gmail.com>
2020-07-04 06:15:29 +0300

    Adding, fetching and displaying post

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { importExpr } from '@angular/compiler/src/output/output_ast';
 import { pipe } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { FormGroup } from '@angular/forms';
+
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
@@ -10,23 +12,67 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: Post[] = [];
+  editPost = false;
+  postId; string;
+  postIndex; number;
 
-  constructor(private http: HttpClient) {}
+  @ViewChild('postForm') postForm: FormGroup;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.fetchPosts();
   }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onSave(postData: Post) {
     // Send Http request
-    this.http.post('https://demoz-app.firebaseio.com/posts.json', postData)
+    if (this.editPost) {
+      const id = this.postId;
+      const newPost = {
+        ...postData,
+        id
+      };
+      this.http.put<{ name: string }>(`https://demoz-app.firebaseio.com/posts/${id}.json`, newPost)
         .subscribe((response) => {
-          console.log(response);
+          this.loadedPosts[this.postIndex] = newPost;
+          this.onClearForm();
         },
+          (error) => {
+            console.error('Failed to update post: ', error.message);
+          });
+    } else {
+      this.http.post<{ name: string }>('https://demoz-app.firebaseio.com/posts.json', postData)
+        .subscribe((response) => {
+          const id = response.name;
+          this.loadedPosts.push({
+            ...postData,
+            id
+          });
+          this.onClearForm();
+        },
+          (error) => {
+            console.error('Failed to save post: ', error.message);
+          });
+    }
+  }
+
+  onDelete() {
+    this.http.delete<{ name: string }>(`https://demoz-app.firebaseio.com/posts/${this.postId}.json`)
+      .subscribe((response) => {
+        this.loadedPosts.splice(this.postIndex, 1);
+        this.onClearForm();
+      },
         (error) => {
-          console.error('Failed to save post: ', error.message);
+          console.error('Failed to delete post: ', error.message);
         });
+  }
+
+  onClearForm() {
+    this.editPost = false;
+    this.postId = undefined;
+    this.postIndex = undefined;
+    this.postForm.reset();
   }
 
   onFetchPosts() {
@@ -37,19 +83,31 @@ export class AppComponent implements OnInit {
     // Send Http request
   }
 
-  fetchPosts() {
-    this.http.get('https://demoz-app.firebaseio.com/posts.json')
-      .pipe(map(response => {
-        const responseArray = [];
+  private fetchPosts() {
+    this.http.get<{ [key: string]: Post }>('https://demoz-app.firebaseio.com/posts.json')
+      .pipe(map((response) => {
+        const responseArray: Post[] = [];
         for (const key in response) {
           if (response.hasOwnProperty(key)) {
-            responseArray.push({...response[key], id: key});
+            responseArray.push({ ...response[key], id: key });
           }
         }
         return responseArray;
       }))
       .subscribe((response) => {
-      console.log(response);
-    });
+        this.updatePostList(response);
+      });
+  }
+
+  updatePostList(posts: Post[]) {
+    this.loadedPosts = posts;
+  }
+
+  onSelect(index: number) {
+    this.editPost = true;
+    this.postIndex = index;
+    const { title, content, id } = this.loadedPosts[index];
+    this.postId = id;
+    this.postForm.setValue({ title, content });
   }
 }
